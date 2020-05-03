64c4fd53e410199368020c49b519ebde7daa0bb2
NicoleKuong <nicolekuong@gmail.com>
2020-05-03 18:20:44 +0200

    install dependencies and create joinChat component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import JoinChat from "./components/JoinChat";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact component={JoinChat} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
